4acc344638dddd50385932a57bf862762d665f84
Delarta Tok Adin <delarta.tok@gmail.com>
2019-03-26 15:46:14 +0700

    [Delarta] - file structure

---
+++ src/pages/ClassDetailPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ClassDetailPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ClassDetailPage;
