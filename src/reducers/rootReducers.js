2b90dbb8654e9861aa6e7db1c098d6f530cc0cd1
danulagi90 <danulagi@gmail.com>
2019-04-03 20:38:36 +0700

    [Danu] - creating category page

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -30,6 +30,26 @@ const initState = {
       name: "Programming",
       image: "ti-desktop"
     },
+    {
+      id: 5,
+      name: "Business",
+      image: "ti-money"
+    },
+    {
+      id: 6,
+      name: "Design",
+      image: "ti-blackboard"
+    },
+    {
+      id: 7,
+      name: "Language",
+      image: "ti-comment-alt"
+    },
+    {
+      id: 8,
+      name: "Mathematic",
+      image: "ti-ruler-alt-2"
+    },
   ],
   classes: [
     {
@@ -45,7 +65,7 @@ const initState = {
       id: 2,
       name: "Face Illustration",
       mentor: "Ade Rahman",
-      category: "design",
+      category: "art",
       fee: 50000,
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam eveniet quae, harum veritatis voluptate reiciendis dignissimos! Voluptates, in. Sunt accusantium quaerat exercitationem tenetur minima id soluta fuga ullam officiis omnis, laboriosam reprehenderit eligendi molestiae sapiente vel optio similique voluptatibus eos, necessitatibus eveniet, quibusdam quisquam magnam amet dolores! Nulla, totam."
@@ -61,9 +81,9 @@ const initState = {
     },
     {
       id: 4,
-      name: "Reog Dance",
+      name: "Basic React Native",
       mentor: "Andy Setiawan",
-      category: "dance",
+      category: "programming",
       fee: 70000,
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam eveniet quae, harum veritatis voluptate reiciendis dignissimos! Voluptates, in. Sunt accusantium quaerat exercitationem tenetur minima id soluta fuga ullam officiis omnis, laboriosam reprehenderit eligendi molestiae sapiente vel optio similique voluptatibus eos, necessitatibus eveniet, quibusdam quisquam magnam amet dolores! Nulla, totam."
@@ -79,13 +99,31 @@ const initState = {
     },
     {
       id: 6,
-      name: "Abstract Painting",
+      name: "Basic Persuasive",
       mentor: "Zakarias",
-      category: "art",
+      category: "business",
       fee: 90000,
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam eveniet quae, harum veritatis voluptate reiciendis dignissimos! Voluptates, in. Sunt accusantium quaerat exercitationem tenetur minima id soluta fuga ullam officiis omnis, laboriosam reprehenderit eligendi molestiae sapiente vel optio similique voluptatibus eos, necessitatibus eveniet, quibusdam quisquam magnam amet dolores! Nulla, totam."
-    }
+    },
+    {
+      id: 7,
+      name: "British Conversation",
+      mentor: "Meggy",
+      category: "language",
+      fee: 100000,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam eveniet quae, harum veritatis voluptate reiciendis dignissimos! Voluptates, in. Sunt accusantium quaerat exercitationem tenetur minima id soluta fuga ullam officiis omnis, laboriosam reprehenderit eligendi molestiae sapiente vel optio similique voluptatibus eos, necessitatibus eveniet, quibusdam quisquam magnam amet dolores! Nulla, totam."
+    },
+    {
+      id: 8,
+      name: "Algebra",
+      mentor: "Liliana",
+      category: "mathematic",
+      fee: 90000,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam eveniet quae, harum veritatis voluptate reiciendis dignissimos! Voluptates, in. Sunt accusantium quaerat exercitationem tenetur minima id soluta fuga ullam officiis omnis, laboriosam reprehenderit eligendi molestiae sapiente vel optio similique voluptatibus eos, necessitatibus eveniet, quibusdam quisquam magnam amet dolores! Nulla, totam."
+    },
   ]
 };
 
