cb2044d7f58b42342703dab707fd246c5e85ae8e
Delarta Tok Adin <delarta.tok@gmail.com>
2019-04-06 18:46:50 +0700

    [Delarta] - polishing the style

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -6,10 +6,9 @@ const initState = {
 
 const mainReducer = (state = initState, action) => {
   switch (action.type) {
-    
     default:
       return state;
   }
 };
 
-export default mainReducer
+export default mainReducer;
