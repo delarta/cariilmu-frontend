4aee89faf5bbf8bccf27f00e7018e1760aa65090
Delarta Tok Adin <delarta.tok@gmail.com>
2019-04-07 19:33:44 +0700

    [Delarta] - deploying heroku app

---
+++ build/precache-manifest.ba0efd1baf758c53b05077e824156b8b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc6245318e481c587af2",
+    "url": "/static/css/main.6926856c.chunk.css"
+  },
+  {
+    "revision": "cc6245318e481c587af2",
+    "url": "/static/js/main.aa7ce5b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18f70131c4e9efffc191",
+    "url": "/static/css/2.3a5617d4.chunk.css"
+  },
+  {
+    "revision": "18f70131c4e9efffc191",
+    "url": "/static/js/2.72f7e266.chunk.js"
+  },
+  {
+    "revision": "cd878f6d3253d0f919d6c0091cb8c5d7",
+    "url": "/static/media/logo_light_inline.cd878f6d.png"
+  },
+  {
+    "revision": "84cd24a754f6421f40507ec4fd408f3c",
+    "url": "/static/media/thinking.84cd24a7.svg"
+  },
+  {
+    "revision": "f616fc1a687304c6ac9321ff6be31b59",
+    "url": "/static/media/ipad.f616fc1a.svg"
+  },
+  {
+    "revision": "b6f1f4be339bbadbfede37be3a9faba5",
+    "url": "/static/media/logo_dark.b6f1f4be.png"
+  },
+  {
+    "revision": "95ac55e3a2528c43a887bac80f195b34",
+    "url": "/static/media/art.95ac55e3.jpg"
+  },
+  {
+    "revision": "6d847bc067ff47f2e7accfa87e6a9b95",
+    "url": "/static/media/presentation.6d847bc0.svg"
+  },
+  {
+    "revision": "27f4d5cdabbf025077124b74285c4af2",
+    "url": "/static/media/gift.27f4d5cd.svg"
+  },
+  {
+    "revision": "819082532d04cad911b6a288cfa8bb68",
+    "url": "/static/media/schedule.81908253.jpg"
+  },
+  {
+    "revision": "23ab5bb4aac4cb600a9e5cec282fb14f",
+    "url": "/static/media/banner.23ab5bb4.mp4"
+  },
+  {
+    "revision": "06937f536c486821b69c3b8974f4b50d",
+    "url": "/static/media/teach.06937f53.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "436aeb1174685518933de529386f6dff",
+    "url": "/static/media/study.436aeb11.svg"
+  },
+  {
+    "revision": "314c762ce636926611aaf119c42206f8",
+    "url": "/static/media/group.314c762c.jpg"
+  },
+  {
+    "revision": "3224ae5e979e72b550691fac0a97967c",
+    "url": "/static/media/banner.3224ae5e.jpg"
+  },
+  {
+    "revision": "199820226ea63b59b54cff63f65c1bac",
+    "url": "/static/media/logodesign.19982022.jpg"
+  },
+  {
+    "revision": "10a6920ebc0a1c281e90c9c74244ceca",
+    "url": "/static/media/music.10a6920e.jpg"
+  },
+  {
+    "revision": "a069d4538ae4c8255e7b5f7ee21e7b37",
+    "url": "/static/media/sport.a069d453.jpg"
+  },
+  {
+    "revision": "eddd92c0606796aedbfbb7ed965464dd",
+    "url": "/index.html"
+  }
+];
