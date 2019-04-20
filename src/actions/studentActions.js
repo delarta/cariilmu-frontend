d52554270af290a478d59f2db6a34fe849a904eb
harun5897 <harungb025@gmail.com>
2019-04-20 13:06:23 +0700

    [Harun] - keep update

--- src/actions/studentActions.js
+++ src/actions/studentActions.js
@@ -46,3 +46,93 @@ export const signUp = (name, username, email, password) => {
       });
   };
 };
+
+
+export const getStudentData = () => {
+  return dispatch => {
+    axios({
+      method: 'get',
+      url: `${url}/student`,
+      headers: { Authorization: localStorage.getItem("token") }
+    })
+      .then(res => {
+        console.log(res)
+        dispatch({
+          type: "FETCH_STUDENT_DATA",
+          payload: res.data.data
+        })
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+};
+
+export const getCart = () => {
+  return dispatch => {
+    axios({
+      method: "get",
+      url: `${url}/student/payment`,
+      headers: { Authorization: localStorage.getItem("token") }
+    })
+      .then(res => {
+        dispatch({
+          type: "FETCH_CART",
+          payload: res.data.data
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+};
+
+export const enroll = (classid) => {
+  console.log('from thunk',classid)
+  return dispatch => {
+    axios({
+      method: "put",
+      url: `${url}/student/class/${classid.classId}/enroll`,
+      headers: { Authorization: localStorage.getItem("token") }
+
+    })
+      .then(res => {
+        console.log("berhasil")
+        dispatch({
+          type: "ENROLL_CLASS",
+          payload: res.data.data
+          
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+};
+
+export const confirmPayment = (classid, photo) => { 
+  console.log('from thunk',classid)
+  console.log('photo', photo)
+  return dispatch => {
+    let bodyFormData = new FormData();
+
+    bodyFormData.append("photo", photo);
+
+    axios({
+      method: "put",
+      url: `${url}/student/payment/${classid}/confirm`,
+      headers: { Authorization: localStorage.getItem("token"),
+      
+    },
+
+      data: bodyFormData
+
+    })
+      .then(res => {
+        console.log("berhasil")
+        dispatch({
+          type: "CONFIRM_PAYMENT",
+          payload : res.data.data
+          
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+};
+
