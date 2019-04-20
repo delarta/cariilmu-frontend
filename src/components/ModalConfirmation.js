72516b23c2fb61eb53a126716ad214faaa6229fa
harun5897 <harungb025@gmail.com>
2019-04-20 13:15:26 +0700

    [Harun] - keep update

--- src/components/ModalConfirmation.js
+++ src/components/ModalConfirmation.js
@@ -1,133 +1,176 @@
-import React, { Component } from "react";
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Input,
-  Label,
-  Form,
-  FormGroup,
-  Col,
-  Button
-} from "reactstrap";
+import React, { Component } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup, Col, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import { confirmPayment } from '../actions/studentActions';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default class ModalConfirmation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      backdrop: true,
-      loading: false
-    };
+class ModalConfirmation extends Component {
+	constructor(props) {
+		super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.changeBackdrop = this.changeBackdrop.bind(this);
-    // this.loading = this.loading.bind(this);
-  }
+		this.state = {
+			modal: false,
+			backdrop: true,
+			loading: false,
+			photo: ''
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
 
-  changeBackdrop(e) {
-    let value = e.target.value;
-    if (value !== "static") {
-      value = JSON.parse(value);
-    }
-    this.setState({ backdrop: value });
-  }
+		
+		};
 
-  render() {
-    return (
-      <div>
-        <Button color="danger" onClick={this.toggle}>
-          {" "}
-          Unpaid
-        </Button>
+		this.toggle = this.toggle.bind(this);
+		this.changeBackdrop = this.changeBackdrop.bind(this);
+	}
 
-        <Modal
-          isOpen={this.state.modal}
-          toggle={this.toggle}
-          className={this.props.className}
-          backdrop={this.state.backdrop}
-        >
-          <ModalHeader toggle={this.toggle}> CONFIRMATION</ModalHeader>
-          <ModalBody>
-            <Form>
-            <FormGroup row>
-              <Label for="id" sm={3}>
-                ID Tranaksi
-              </Label>
-              <Col sm={9}>
-                <Input type="id" name="id" id="id" placeholder="" />
-              </Col>
-            </FormGroup>
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal,
+		}));
+	}
 
-            <FormGroup row>
-              <Label for="transfer" sm={3}>
-                Transfer To
-              </Label>
-              <Col sm={9}>
-                <Input
-                  type="transfer"
-                  name="transfer"
-                  id="transfer"
-                  placeholder=""
-                />
-              </Col>
-            </FormGroup>
+	changeBackdrop(e) {
+		let value = e.target.value;
+		if (value !== 'static') {
+			value = JSON.parse(value);
+		}
+		this.setState({ backdrop: value });
+	}
 
-            <FormGroup row>
-              <Label for="price" sm={3}>
-                Price
-              </Label>
-              <Col sm={9}>
-                <Input type="price" name="price" id="price" placeholder="" />
-              </Col>
-            </FormGroup>
+	handlePayment = classid => {
+		classid.preventDefault();
+		this.props.confirmPayment(this.props.id_payment, this.state.photo);
+		
+		
+	};
 
-            <FormGroup row>
-              <Label for="code" sm={3}>
-                Code
-              </Label>
-              <Col sm={9}>
-                <Input type="code" name="code" id="code" placeholder="" />
-              </Col>
-            </FormGroup>
+	getPhoto = e => {
+		e.preventDefault();
+		this.setState({
+		photo: e.target.files[0]
+		});
+	  };
 
-            <FormGroup row>
-              <Label for="total" sm={3}>
-                Total
-              </Label>
-              <Col sm={9}>
-                <Input type="total" name="total" id="total" placeholder="" />
-              </Col>
-            </FormGroup>
+	render() {
+		const  classId  = this.props.id_payment;
+		return (
+			<div>
+				<Button color="danger" onClick={this.toggle} block>
+					Unpaid
+				</Button>
 
-            <FormGroup row>
-              <Label for="exampleFile" sm={3}>
-                File
-              </Label>
-              <Col sm={9}>
-                <Input type="file" name="file" id="exampleFile" />
-                <label htmlFor="exampleFile">upload</label>
-              </Col>
-            </FormGroup>
-            </Form>
-          </ModalBody>
-          <ModalFooter>
-            <Button className="btn btn-primary" onClick={this.toggle}>
-              {" "}
-              Confirm
-            </Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+				<Modal
+					isOpen={this.state.modal}
+					toggle={this.toggle}
+					className={this.props.className}
+					backdrop={this.state.backdrop}
+				>
+					{
+						this.props.students
+						.filter(item => item.class._id === classId)
+						.map(item => (
+							<Form key={item.class._id} onSubmit={this.handlePayment}>
+								<ModalHeader toggle={this.toggle}> CONFIRMATION</ModalHeader>
+								<ModalBody>
+									<Form>
+										<FormGroup row>
+											<Label for="id" sm={3}>
+												ID Transaksi
+											</Label>
+											<Col sm={9}>
+												<Input
+													type="id"
+													name="id"
+													id="id"
+													placeholder=""
+													value={item.class._id}
+												/>
+											</Col>
+										</FormGroup>
+
+										<FormGroup row>
+											<Label for="item" sm={3}>
+												Item
+											</Label>
+											<Col sm={9}>
+												<Input
+													type="item"
+													name="item"
+													id="item"
+													placeholder=""
+													value={item.class.name}
+												/>
+											</Col>
+										</FormGroup>
+
+										<FormGroup row>
+											<Label for="price" sm={3}>
+												Price
+											</Label>
+											<Col sm={9}>
+												<Input
+													type="price"
+													name="price"
+													id="price"
+													placeholder=""
+													value={item.class.fee}
+												/>
+											</Col>
+										</FormGroup>
+
+										<FormGroup row>
+											<Label for="bank" sm={3}>
+												Bank
+											</Label>
+											<Col sm={9}>
+												<Input type="bank" name="bank" id="bank" placeholder="" />
+											</Col>
+										</FormGroup>
+
+										<FormGroup row>
+											<Label for="rek" sm={3}>
+												No Rek
+											</Label>
+											<Col sm={9}>
+												<Input type="rek" name="rek" id="rek" placeholder="" />
+											</Col>
+										</FormGroup>
+
+										<FormGroup row>
+											<Label for="exampleFile" sm={3}>
+												File
+											</Label>
+											<Col sm={9}>
+												<Input type="file" name="file" id="exampleFile" onChange={this.getPhoto}/>
+												<label htmlFor="exampleFile">upload</label>
+											</Col>
+										</FormGroup>
+									</Form>
+								</ModalBody>
+								<ModalFooter>
+									<Button type="submit" color="primary">
+										Submit
+									</Button>
+								</ModalFooter>
+							</Form>
+						))}
+				</Modal>
+			</div>
+		);
+	}
 }
+const mapStateToProps = state => {
+	return {
+		students: state.main.students,
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		confirmPayment: (classid, photo) => dispatch(confirmPayment(classid, photo)),
+	};
+};
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ModalConfirmation);
